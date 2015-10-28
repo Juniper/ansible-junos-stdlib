9e2e3a5f04d32b4a36c2ee38bcdf2557c9ba997e
spidercensus <jpack@juniper.net>
2015-10-28 14:17:29 -0400

    improved documentation in inventory.py

--- tools/inventory.py
+++ tools/inventory.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # inventory.py
+# by Jason Pack
 # returns the list of devices managed by Space as an inventory for Ansible.
 
 import requests
@@ -13,24 +14,26 @@ import sys
 from pprint import pprint as pp
 requests.packages.urllib3.disable_warnings() #disable insecure request warnings from urllib3.
 def retrieve(url, user, password, postData=None, headers={}):
-    returnVal = None
-    if "https://" not in url:
-        url =  "https://" + url
-    r = None
-    sys.stderr.write("Requesting %s..\n" % url);
-    if postData is None:
-        r = requests.get(url, verify=False, auth=(user, password), headers=headers)
-    else:
-        r = requests.post(url, verify=False, auth=(user, password), headers=headers, data=postData)
-    if r.status_code!=200 and r.status_code!=204:
-        raise Exception("Got status code %d while retrieving %s!\n" % (r.status_code, url))
-    return r.content
+  '''Retrieve some HTTP-accessible resource.'''
+  returnVal = None
+  if "https://" not in url:
+    url =  "https://" + url
+  r = None
+  sys.stderr.write("Requesting %s..\n" % url);
+  if postData is None:
+    r = requests.get(url, verify=False, auth=(user, password), headers=headers)
+  else:
+    r = requests.post(url, verify=False, auth=(user, password), headers=headers, data=postData)
+  if r.status_code!=200 and r.status_code!=204:
+    raise Exception("Got status code %d while retrieving %s!\n" % (r.status_code, url))
+  return r.content
 
 def xmlize(content):
-    try:
-        return XML(content)
-    except:
-        return None
+  '''Attempt to convert something to XML. otherwise return none.'''
+  try:
+    return XML(content)
+  except:
+    return None
 
 class Space:
   def __init__(self, config_filename='junos_space.json'):
@@ -72,92 +75,96 @@ class Space:
     self.domains = None
     
   def getDevices(self):
+    '''Return the devices managed by this Space instance.'''
     if self.devices is None:
       self.updateDevices()
     return self.devices
+    
   def getDomains(self):
+    '''Return the domains managed by this Space instance.'''
     if self.domains is None:
       self.updateDomains()
     return self.domains
   def updateDomains(self, domain_id=2):
-      url = "https://" + self.host + "/api/space/domain-management/domains/" + str(domain_id)
-      data = retrieve(url, user=self.user, password=self.password);
-      domains = {}
-      xml = xmlize(data)
-      if xml is None:
-          return domains
-      name = xml.find('name').text
-      id = xml.find('id').text
-      new_domain = {}
-      new_domain['id'] = id
-      new_domain['name'] = name
-      domains[id] = new_domain
-      if xml.find('child-count') is not None:
-          if int(xml.find('child-count').text) > 0:
-              for i in xml.findall('children/domain'):
-                  child_id = i.find('id').text
-                  if int(i.find('child-count').text) > 0:
-                      #babies havin babies
-                      children = self.updateDomains(domain_id = child_id)
-                      domains.update(children)
-                  else:
-                      new_domain = {}
-                      new_domain['id'] = child_id
-                      new_domain['name'] = i.find('name').text
-                      domains[child_id]  = new_domain
-      self.domains = domains
+    '''Get info about all domains managed by the Space instance.'''
+    url = "https://" + self.host + "/api/space/domain-management/domains/" + str(domain_id)
+    data = retrieve(url, user=self.user, password=self.password);
+    domains = {}
+    xml = xmlize(data)
+    if xml is None:
       return domains
+    name = xml.find('name').text
+    id = xml.find('id').text
+    new_domain = {}
+    new_domain['id'] = id
+    new_domain['name'] = name
+    domains[id] = new_domain
+    if xml.find('child-count') is not None:
+      if int(xml.find('child-count').text) > 0:
+        for i in xml.findall('children/domain'):
+          child_id = i.find('id').text
+          if int(i.find('child-count').text) > 0:
+            #babies havin babies
+            children = self.updateDomains(domain_id = child_id)
+            domains.update(children)
+        else:
+            new_domain = {}
+            new_domain['id'] = child_id
+            new_domain['name'] = i.find('name').text
+            domains[child_id]  = new_domain
+    self.domains = domains
+    return domains
 
   def updateDevices(self, device_filter=None):
-      domains  = self.getDomains()
-      devices = {}
-      if device_filter is None:
-          for d_id in domains.keys():
-              new_devices = self.updateDevices( device_filter="(domain-id eq '" + d_id + "')")
-              for (dev_id, device) in new_devices.items():
-                  device['domain_id'] = d_id
-              devices.update(new_devices)
-          return devices;
-      url = "https://" + self.host + "/api/space/device-management/devices?filter=" + device_filter + "&"
-      pageOffset = 0;
-      pageMax = 1000;
-      xmlDevs = [];
-      numDevices = 9999
-      while pageOffset < numDevices:
-          #print "%d is less than %d" % (pageOffset, numDevices)
-          theseDevs = retrieve(url + "paging=(start eq "+str(pageOffset)+", limit eq "+str(pageMax)+ ")",user=self.user, password=self.password);
-          theseDevs = xmlize(theseDevs)
-          if theseDevs is None:
-              break
-          xmlDevs = xmlDevs + theseDevs.getchildren()
-          pageOffset = pageOffset + pageMax
-      for xml in xmlDevs:
-          platform = xml.find('platform')
-          if (platform is None):
-              sys.stderr.write("%s: Unable to determine platform.  It will not be included in inventory.\n" % xml.find('name').text)
-              continue;
-          version = xml.find('OSVersion')
-          if version is None:
-              sys.stderr.write("%s: Unable to determine Junos version.  It will not be included in inventory.\n" % xml.find('name').text)
-              continue;
-          ip = xml.find('ipAddr')
-          if ip is None:
-              sys.stderr.write("%s: Unable to determine IP address. It will not be included in inventory.\n" % xml.find('name').text)
-              continue;
-          devId = xml.get('key')
-          device = {'name'  :  xml.find('name').text,
-                    'id'    :  devId,
-                    'platform':  platform.text,
-                    'version'  :  version.text,
-                    'ip'      :  ip.text}
-          devices[devId] = device
-      self.devices = devices
-      return devices;  
-      
+    '''Get info about all devices managed by the Space instance.'''
+    domains  = self.getDomains()
+    devices = {}
+    if device_filter is None:
+        for d_id in domains.keys():
+            new_devices = self.updateDevices( device_filter="(domain-id eq '" + d_id + "')")
+            for (dev_id, device) in new_devices.items():
+                device['domain_id'] = d_id
+            devices.update(new_devices)
+        return devices;
+    url = "https://" + self.host + "/api/space/device-management/devices?filter=" + device_filter + "&"
+    pageOffset = 0;
+    pageMax = 1000;
+    xmlDevs = [];
+    numDevices = 9999
+    while pageOffset < numDevices:
+      #print "%d is less than %d" % (pageOffset, numDevices)
+      theseDevs = retrieve(url + "paging=(start eq "+str(pageOffset)+", limit eq "+str(pageMax)+ ")",user=self.user, password=self.password);
+      theseDevs = xmlize(theseDevs)
+      if theseDevs is None:
+          break
+      xmlDevs = xmlDevs + theseDevs.getchildren()
+      pageOffset = pageOffset + pageMax
+    for xml in xmlDevs:
+      platform = xml.find('platform')
+      if (platform is None):
+        sys.stderr.write("%s: Unable to determine platform.  It will not be included in inventory.\n" % xml.find('name').text)
+        continue;
+      version = xml.find('OSVersion')
+      if version is None:
+        sys.stderr.write("%s: Unable to determine Junos version.  It will not be included in inventory.\n" % xml.find('name').text)
+        continue;
+      ip = xml.find('ipAddr')
+      if ip is None:
+        sys.stderr.write("%s: Unable to determine IP address. It will not be included in inventory.\n" % xml.find('name').text)
+        continue;
+      devId = xml.get('key')
+      device = {'name'  :  xml.find('name').text,
+                'id'    :  devId,
+                'platform':  platform.text,
+                'version'  :  version.text,
+                'ip'      :  ip.text}
+      devices[devId] = device
+    self.devices = devices
+    return devices;  
 def export(devices):
   ''' 
-  Creates an ansible host entry for the given device object
-  Expects the device to have  'name' attribute.
+  Creates ansible host entries for the given device objects
+  Expects the devices to have the 'name' attribute.
   All the device's attributes are added as metavars.
   '''
   data = {    "_meta"  :  {
@@ -175,5 +182,4 @@ if __name__ == "__main__":
     space = Space()
     devs = space.getDevices()
     export(devs)
-    #print "There were %d devices" % len(devs.keys())
 
