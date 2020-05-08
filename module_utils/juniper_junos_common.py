b85647efa91bbb4782a9b6116a0aa57c87b7f01c
rahkumar651991 <rahkumar@juniper.net>
2020-05-08 15:56:07 +0530

    merging the earlier changes in single line

--- module_utils/juniper_junos_common.py
+++ module_utils/juniper_junos_common.py
@@ -1570,8 +1570,7 @@ class JuniperJunosModule(AnsibleModule):
 
         self.logger.debug("Diffing candidate and committed configurations.")
         try:
-            rb_id =0
-            diff = self.config.diff(rb_id, ignore_warning)
+            diff = self.config.diff(rb_id=0, ignore_warning=ignore_warning)
             self.logger.debug("Configuration diff completed.")
             return diff
         except (self.pyez_exception.RpcError,
