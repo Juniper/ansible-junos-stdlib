0cef75e862dc58e5bc86f079ba18eab2695fddfe
Dinesh babu <dineshbaburam91@rediffmail.com>
2024-01-25 11:56:00 +0530

    Issue:  commit_configuration API parameters are missing in ansible Py… (#638)
    * Issue:  commit_configuration API parameters are missing in ansible PyEZ connection API  and also formatted error was noticed.
    This impact committing the configuration using ansible PyEZ connection.
    
    Fix:
    Supported valid parameter in commit_configuration API parameter.
    Fixed format string issue
    Fixed configuration mode support for ansible PyEZ connection.
    
    * Issue:  commit_configuration API parameters are missing in ansible PyEZ connection API  and also formatted error was noticed.
    This impact committing the configuration using ansible PyEZ connection.
    
    Fix:
    Supported valid parameter in commit_configuration API parameter.
    Fixed format string issue
    Fixed configuration mode support for ansible PyEZ connection.
    
    * Fix

--- ansible_collections/juniper/device/plugins/module_utils/juniper_junos_common.py
+++ ansible_collections/juniper/device/plugins/module_utils/juniper_junos_common.py
@@ -1165,7 +1165,9 @@ class JuniperJunosModule(AnsibleModule):
             if mode not in CONFIG_MODE_CHOICES:
                 self.fail_json(msg='Invalid configuration mode: %s' % (mode))
             if mode != 'ephemeral' and ephemeral_instance is not None:
-                self.fail_json(msg='configuration mode ephemeral is required')
+                self.fail_json(msg='Ephemeral instance is specified while the mode '
+                               'is not ephemeral.Specify the mode as ephemeral or '
+                               'do not specify the instance.')
             if self.dev is None:
                 self.open()
             config = jnpr.junos.utils.config.Config(self.dev, mode=mode)
@@ -1542,7 +1544,7 @@ class JuniperJunosModule(AnsibleModule):
             - An error returned from committing the configuration.
         """
         if self.conn_type != "local":
-            self._pyez_conn.commit_configuration(ignore_warning, comment, timeout, confirmed)
+            self._pyez_conn.commit_configuration(ignore_warning, comment, timeout, confirmed, full, sync, force_sync)
             return
 
         if self.dev is None or self.config is None:
