22b816ee5d15b8ee1b095ebdc299b3bf807f4e38
chidanandpujar <chidanandpujar@gmail.com>
2025-03-04 15:30:37 +0530

    module redirection check

---
+++ ansible_collections/juniper/device/plugins/module_utils/network/junos/facts/facts.py
@@ -0,0 +1,203 @@
+#
+# -*- coding: utf-8 -*-
+# Copyright 2019 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The facts class for junos
+this file validates each subset of facts and selectively
+calls the appropriate facts gathering function
+"""
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.facts.facts import (
+    FactsBase,
+)
+
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.acl_interfaces.acl_interfaces import (
+    Acl_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.acls.acls import (
+    AclsFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.bgp_address_family.bgp_address_family import (
+    Bgp_address_familyFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.bgp_global.bgp_global import (
+    Bgp_globalFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.hostname.hostname import (
+    HostnameFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.interfaces.interfaces import (
+    InterfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.l2_interfaces.l2_interfaces import (
+    L2_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.l3_interfaces.l3_interfaces import (
+    L3_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.lacp.lacp import (
+    LacpFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.lacp_interfaces.lacp_interfaces import (
+    Lacp_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.lag_interfaces.lag_interfaces import (
+    Lag_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.legacy.base import (
+    Config,
+    Default,
+    Hardware,
+    Interfaces,
+    OFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.lldp_global.lldp_global import (
+    Lldp_globalFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.lldp_interfaces.lldp_interfaces import (
+    Lldp_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.logging_global.logging_global import (
+    Logging_globalFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.ntp_global.ntp_global import (
+    Ntp_globalFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.ospf_interfaces.ospf_interfaces import (
+    Ospf_interfacesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.ospfv2.ospfv2 import (
+    Ospfv2Facts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.ospfv3.ospfv3 import (
+    Ospfv3Facts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.prefix_lists.prefix_lists import (
+    Prefix_listsFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.routing_instances.routing_instances import (
+    Routing_instancesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.routing_options.routing_options import (
+    Routing_optionsFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.security_policies.security_policies import (
+    Security_policiesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.security_policies_global.security_policies_global import (
+    Security_policies_globalFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.security_zones.security_zones import (
+    Security_zonesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.snmp_server.snmp_server import (
+    Snmp_serverFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.static_routes.static_routes import (
+    Static_routesFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.vlans.vlans import (
+    VlansFacts,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.junos import (
+    HAS_PYEZ,
+)
+
+
+FACT_LEGACY_SUBSETS = dict(
+    default=Default,
+    hardware=Hardware,
+    config=Config,
+    interfaces=Interfaces,
+)
+FACT_RESOURCE_SUBSETS = dict(
+    acls=AclsFacts,
+    acl_interfaces=Acl_interfacesFacts,
+    interfaces=InterfacesFacts,
+    lacp=LacpFacts,
+    lacp_interfaces=Lacp_interfacesFacts,
+    lag_interfaces=Lag_interfacesFacts,
+    l2_interfaces=L2_interfacesFacts,
+    l3_interfaces=L3_interfacesFacts,
+    lldp_global=Lldp_globalFacts,
+    lldp_interfaces=Lldp_interfacesFacts,
+    ospfv2=Ospfv2Facts,
+    ospfv3=Ospfv3Facts,
+    ospf_interfaces=Ospf_interfacesFacts,
+    vlans=VlansFacts,
+    static_routes=Static_routesFacts,
+    bgp_global=Bgp_globalFacts,
+    bgp_address_family=Bgp_address_familyFacts,
+    routing_instances=Routing_instancesFacts,
+    prefix_lists=Prefix_listsFacts,
+    logging_global=Logging_globalFacts,
+    ntp_global=Ntp_globalFacts,
+    security_policies=Security_policiesFacts,
+    security_policies_global=Security_policies_globalFacts,
+    security_zones=Security_zonesFacts,
+    snmp_server=Snmp_serverFacts,
+    routing_options=Routing_optionsFacts,
+    hostname=HostnameFacts,
+)
+
+
+class Facts(FactsBase):
+    """The fact class for junos"""
+
+    VALID_LEGACY_GATHER_SUBSETS = frozenset(FACT_LEGACY_SUBSETS.keys())
+    VALID_RESOURCE_SUBSETS = frozenset(FACT_RESOURCE_SUBSETS.keys())
+
+    def __init__(self, module):
+        super(Facts, self).__init__(module)
+
+    def get_facts(
+        self,
+        legacy_facts_type=None,
+        resource_facts_type=None,
+        data=None,
+    ):
+        """Collect the facts for junos
+        :param legacy_facts_type: List of legacy facts types
+        :param resource_facts_type: List of resource fact types
+        :param data: previously collected conf
+        :rtype: dict
+        :return: the facts gathered
+        """
+        if self.VALID_RESOURCE_SUBSETS:
+            self.get_network_resources_facts(
+                FACT_RESOURCE_SUBSETS,
+                resource_facts_type,
+                data,
+            )
+
+        if not legacy_facts_type:
+            legacy_facts_type = self._gather_subset
+            # fetch old style facts only when explicitly mentioned in gather_subset option
+            if "ofacts" in legacy_facts_type:
+                if HAS_PYEZ:
+                    self.ansible_facts.update(OFacts(self._module).populate())
+                else:
+                    self._warnings.extend(
+                        [
+                            "junos-eznc is required to gather old style facts but does not appear to be installed. "
+                            "It can be installed using `pip install junos-eznc`",
+                        ],
+                    )
+                self.ansible_facts["ansible_net_gather_subset"].append(
+                    "ofacts",
+                )
+                legacy_facts_type.remove("ofacts")
+
+        if self.VALID_LEGACY_GATHER_SUBSETS:
+            self.get_network_legacy_facts(
+                FACT_LEGACY_SUBSETS,
+                legacy_facts_type,
+            )
+
+        return self.ansible_facts, self._warnings
---
+++ ansible_collections/juniper/device/plugins/modules/junos_facts.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017, Ansible by Red Hat, inc
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+
+DOCUMENTATION = """
+module: junos_facts
+author: Nathaniel Case (@Qalthos)
+short_description: Collect facts from remote devices running Juniper Junos
+description:
+- Collects fact information from a remote device running the Junos operating system.  By
+  default, the module will collect basic fact information from the device to be included
+  with the hostvars. Additional fact information can be collected based on the configured
+  set of arguments.
+version_added: 1.0.0
+extends_documentation_fragment:
+- junipernetworks.junos.junos
+options:
+  gather_subset:
+    description:
+    - When supplied, this argument will restrict the facts collected to a given subset.  Possible
+      values for this argument include C(all), C(hardware), C(config), C(interfaces) and C(min). Can
+      specify a list of values to include a larger subset.  Values can also be used
+      with an initial C(!) to specify that a specific subset should not be collected.
+      To maintain backward compatibility old style facts can be retrieved by explicitly
+      adding C(ofacts)  to value, this requires junos-eznc to be installed as a prerequisite.
+      Valid value of gather_subset are default, hardware, config, interfaces, ofacts.
+      If C(ofacts) is present in the list it fetches the old style facts (fact keys
+      without 'ansible_' prefix) and it requires junos-eznc library to be installed.
+    required: false
+    default:
+    - 'min'
+    type: list
+    elements: str
+  config_format:
+    description:
+    - The I(config_format) argument specifies the format of the configuration when
+      serializing output from the device. This argument is applicable only when C(config)
+      value is present in I(gather_subset). The I(config_format) should be supported
+      by the junos version running on device. This value is not applicable while fetching
+      old style facts that is when C(ofacts) value is present in value if I(gather_subset)
+      value. This option is valid only for C(gather_subset) values.
+    type: str
+    required: false
+    default: text
+    choices:
+    - xml
+    - text
+    - set
+    - json
+  gather_network_resources:
+    description:
+    - When supplied, this argument will restrict the facts collected to a given subset.
+      Possible values for this argument include all and the resources like interfaces,
+      vlans etc. Can specify a list of values to include a larger subset. Values can
+      also be used with an initial C(!) to specify that a specific subset should
+      not be collected. Valid subsets are 'all', 'interfaces', 'lacp', 'lacp_interfaces',
+      'lag_interfaces', 'l2_interfaces', 'l3_interfaces', 'lldp_global', 'lldp_interfaces',
+      'vlans'.
+    required: false
+    type: list
+    elements: str
+  available_network_resources:
+    description: When 'True' a list of network resources for which resource modules are available will be provided.
+    type: bool
+    default: false
+requirements:
+- ncclient (>=v0.5.2)
+notes:
+- Ensure I(config_format) used to retrieve configuration from device is supported
+  by junos version running on device.
+- With I(config_format = json), configuration in the results will be a dictionary(and
+  not a JSON string)
+- This module requires the netconf system service be enabled on the remote device
+  being managed.
+- Tested against vSRX JUNOS version 15.1X49-D15.4, vqfx-10000 JUNOS Version 15.1X53-D60.4.
+- Recommended connection is C(netconf). See L(the Junos OS Platform Options,../network/user_guide/platform_junos.html).
+- This module also works with C(local) connections for legacy playbooks.
+"""
+
+EXAMPLES = """
+- name: collect default set of facts
+  junipernetworks.junos.junos_facts:
+
+- name: collect default set of facts and configuration
+  junipernetworks.junos.junos_facts:
+    gather_subset: config
+
+- name: Gather legacy and resource facts
+  junipernetworks.junos.junos_facts:
+    gather_subset: all
+    gather_network_resources: all
+"""
+
+RETURN = """
+ansible_facts:
+  description: Returns the facts collect from the device
+  returned: always
+  type: dict
+"""
+from ansible.module_utils.basic import AnsibleModule
+
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.argspec.facts.facts import (
+    FactsArgs,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.facts import (
+    FACT_RESOURCE_SUBSETS,
+    Facts,
+)
+
+
+def main():
+    """
+    Main entry point for module execution
+
+    :returns: ansible_facts
+    """
+    argument_spec = FactsArgs.argument_spec
+
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    warnings = []
+    ansible_facts = {}
+    if module.params.get("available_network_resources"):
+        ansible_facts["available_network_resources"] = sorted(
+            FACT_RESOURCE_SUBSETS.keys(),
+        )
+    result = Facts(module).get_facts()
+    additional_facts, additional_warnings = result
+    ansible_facts.update(additional_facts)
+    warnings.extend(additional_warnings)
+    module.exit_json(ansible_facts=ansible_facts, warnings=warnings)
+
+
+if __name__ == "__main__":
+    main()
